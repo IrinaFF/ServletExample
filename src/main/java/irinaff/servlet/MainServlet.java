55fc96ae99294bbe749d249a254761cda530504a
IrinaFF <irina.fatkullina@gmail.com>
2017-02-09 12:43:09 +0500

    сервлет, JSP

--- src/main/java/irinaff/servlet/MainServlet.java
+++ src/main/java/irinaff/servlet/MainServlet.java
@@ -1,6 +1,7 @@
 package irinaff.servlet;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,9 +10,15 @@ import java.io.PrintWriter;
 
 /**
  * Как создать Servlet http://devcolibri.com/4284
- * web.xml в этом файле мы зарегистрируем наш сервлет и замапим его на URL
+ * http://tomcat.apache.org/download-80.cgi v 8.5.11
+ * http://apache-mirror.rbc.ru/pub/apache/tomcat/tomcat-8/v8.5.11/bin/apache-tomcat-8.5.11-windows-x64.zip
  * Created by IFatkullina on 08.02.2017.
  */
+//способ регистрации сервлетов без web.xml
+    //Данная аннотация регистрирует сервлет в контексте приложения, это тоже самое что мы делали в web.xml
+    //Аннотация принимает как параметр строку, именно эта строка соответствует тегу url-pattern, тоесть это URL по которому будет доступен сервлет
+    //в pom.xml и в maven-war-plugin добавляем свойство которое делает не обязательным web.xml
+@WebServlet("/")
 //HttpServlet предназначен для написания сервлетов по типу общения клиент-сервер, а именно используя HTTP протокол
 public class MainServlet extends HttpServlet {
     @Override
@@ -21,8 +28,17 @@ public class MainServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         //super.doGet(req, resp);
-        PrintWriter out = resp.getWriter();
-        out.print("<h1>Hello Servlet</h1>");
+        //PrintWriter out = resp.getWriter();
+        //out.print("<h1>Hello Servlet</h1>");
+
+        //Для того чтобы передать какие либо дынные на JSP
+        // нам нужно в сервлете в request добавить атрибут
+        //Чтобы добавить атрибут в request нужно вызвать метод setAttribute(String key, Object value),
+        // где key — это ключ по которому вы получите доступ к данным на JSP;
+        // value — данные которые мы передаем.
+        req.setAttribute("name", "Irinaff");
+
+        req.getRequestDispatcher("mypage.jsp").forward(req, resp);
     }
 
     @Override
---
+++ src/main/webapp/mypage.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: IFatkullina
+  Date: 09.02.2017
+  Time: 12:07
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Irinaff</title>
+</head>
+<body>
+    <h1>Hello ${name}! JSP </h1>
+</body>
+</html>
